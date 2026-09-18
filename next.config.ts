71b84696f4a07b2a5aa96764a381ee9f819f0678
envystilllearning <envystilllearning@users.noreply.github.com>
2026-09-18 11:12:32 +0700

    fix: remove basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/siswa-telatan-v2",
   trailingSlash: true,
   images: { unoptimized: true },
 };
